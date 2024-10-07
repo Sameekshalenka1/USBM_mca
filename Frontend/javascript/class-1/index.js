2db838b78ff46e5d73aa2511ed057f9fa9f7045b
sameeksha <sameekshalenka5@gmail.com>
2024-10-07 13:43:16 +0530

    some changes

---
+++ Frontend/javascript/class-1/index.js
@@ -0,0 +1,101 @@
+let name1="sams"
+console.log(name1);
+
+console.log(address);
+var address = "Bhubaneswar";
+console.log(address);
+
+
+//nul
+let user=null; // Object
+
+// We can't redeclare a variable in a same name
+
+console.log(user+typeof(user));
+console.log(address+typeof(address));
+
+// null- should have no value
+// 2 hours
+
+//undefined
+let data;
+console.log(typeof data);  //undefined
+console.log(data);
+
+//Number
+let num1 = 50;
+let decimal_no = 1.0001;
+let pi = 3.14;
+let age = 25;
+let roll_no = 21;
+
+console.log(typeof num1);   //Number
+
+
+//Addition
+let num2 = 100;
+let num3 = 125;
+let addition = num2 + num3;
+console.log(addition);
+
+
+//substraction
+let num4 = 101;
+let num5 = 15;
+let substraction = num4 - num5;
+console.log(substraction);
+
+
+//Multipication
+let num6 = 355;
+let num7 = 34;
+let Multipication = num6 * num7;
+console.log(Multipication); 
+
+
+//Division
+let num8 = 36;
+let num9 = 6;
+let Division = num8 / num9;
+console.log(Division);     
+
+//Bignit
+let huge_no=34567888888;
+console.log(typeof huge_no);
+
+
+
+
+//NaN
+let num11= 0;
+let num10=0;
+console.log(num11/num10);
+
+
+let n=0;
+let m=10;
+console.log(n/m);
+
+
+//Boolean
+let isMarried = false;
+let isSingle = true;
+console.log(typeof isMarried);
+console.log(typeof isSingle);
+
+
+let name="sameeksha";
+let city="Bhubaneswar";
+let batch="2025";
+let college="USBM";
+//console.log(name);
+//console.log(city);
+//console.log(batch);
+//console.log(college);
+console.log(`My name is: ${name}, city is: ${city}, batch is: ${batch}, and college is: ${college}`);
+
+
+
+
+
+
