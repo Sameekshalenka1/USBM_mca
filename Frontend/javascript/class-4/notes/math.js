b2d3f5946abd4fe05fbc6f668a880f39ad59f6ac
Sameekshalenka1 <sameekshalenka5@gmail.com>
2024-10-28 23:49:09 +0530

    string task added

---
+++ Frontend/javascript/class-4/notes/math.js
@@ -0,0 +1,109 @@
+//Math in js
+
+//Math is an in-built mathemattiical operattion in Js.
+
+/*let num1=25;
+
+let num2=55.05;
+
+let add =num1+num2; console.log(add);
+*/
+//round
+
+console.log(Math.round(12.000012));
+
+console.log(Math.round(add));
+
+console.log(Math.PI);//3.141592653589793
+
+//Square root
+
+console.log(Math.pow(35));//NaN parameter not passed
+
+console.log(Math.pow(35,2)); console.log(Math.pow(35,3));
+
+console.log(num1 ** 2);//625 (power)
+
+console.log(Math.sqrt(16)); //square root
+
+//Ceil
+
+console.log(Math.ceil(98.75));//99
+
+console.log(Math.ceil(325.49));//326
+
+//Floor
+
+console.log(Math.floor(98.75));//98
+
+console.log(Math.floor(325.49));//325
+
+console.log(Math.round(325.50));//326
+
+console.log(Math.round(325.49));//325
+
+//Max
+
+console.log(Math.max(10,20,30,40,50));//50
+
+//Min
+
+console.log(Math.min(10,20,30,40,50));//10
+
+//absolute
+
+console.log(Math.abs(-5));//5
+
+console.log(Math.abs(5));//5
+
+console.log(Math.abs(-0.00005));//0.00005
+
+//random(0 to 1)
+
+console.log(Math.random());
+
+//write a prgrm to print the random number between 5 to 27
+
+//let start=5;
+
+//let end=27;
+
+//((end-start)+1) to find out the numbers
+
+//let tot=(end-start)+1; I
+
+//console.log(Math.random() * tot);//2.432359079666938
+
+//console.log(Math.random() * tot);//14.916504679849151
+
+//5 to 27
+
+///let random_number=Math.floor(Math.random()*tot + start);
+
+//console.log(random_number);
+
+//Dates
+
+//to create date object
+/*
+let current_date=new Date();
+
+console.log(current_date);//2024-10-15T05:42:38.8192
+
+console.log(current_date.torsoString());//2024-10-15T05:54:20.1732
+
+console.log(current_date.getFullYear());//2024
+
+console.log(current_date.getMonth()+1);//10---->oct
+console.log(current_date.getDate()); //15
+console.log(current_date.getHours()); //11
+console.log(current_date.getMinutes()); //24
+console.log(current_date.getSeconds()); //20
+console.log(current_date.getDay()); //2
+
+*/
+
+
+
+
+
