2d7ca3e81c3c11051681a27465958be5f72542a2
Sameekshalenka1 <sameekshalenka5@gmail.com>
2024-10-29 12:15:12 +0530

    string task added

---
+++ Frontend/javascript/class-6/notes/question.js
@@ -0,0 +1,52 @@
+
+//Question: to do difference between push(),pop(),shipt(),unshift() with proper defination and example.
+
+/*push()
+it is a method in javascript. it add the element into last in a array it add multiple element  at a Time.
+
+//ex: 
+let color=["black","white","blue","red"];
+console.log(color);
+let addcolor="pink";
+color.push(addcolor);
+console.log(color);   */
+
+
+/*//pop()
+The pop method is used to remove the element and element will be delete from Last.
+
+//ex :
+let number=["4","45","38","39"];
+console.log(number);
+number.pop();
+console.log(number);
+
+*/
+
+
+/*
+//shift()
+//by using shift(), we also remove the element and element will be delete from first.
+
+//ex :
+let vehicle=["car","motercycle","buses","trucks"];
+console.log(vehicle);
+vehicle.shift();
+console.log(vehicle);
+*/
+
+
+/*
+//unshift()
+//by using this method element will will add in first.
+
+//ex :
+let fruits = ["Apple","banana","mango","orange"];
+console.log(fruits);
+fruits.unshift();
+console.log(fruits);
+
+*/
+
+
+
